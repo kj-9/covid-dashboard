53d37fcc51c5552d0513506a73e07ba65b2987c3
kj002 <kojih9@gmail.com>
2020-08-10 16:04:21 +0900

    refactor simple horizontal bars

--- src/components/simpleHorizontalBars.tsx
+++ src/components/simpleHorizontalBars.tsx
@@ -1,41 +1,64 @@
 import React from 'react';
-import { BarGroupHorizontal, Bar} from '@vx/shape';
 import { Group } from '@vx/group';
-import {SimpleDataType, SimpleKeyType, SimpleKey} from './simpleData'
+import SimpleHorizontalBar from './simpleHorizontalBars';
+import SimpleDataScaler from './simpleDataScaler'
+import { AxisLeft } from '@vx/axis';
+import { SimpleDataType, SimpleKeyType, SimpleKey } from './simpleData'
+import { BarGroupHorizontal, Bar } from '@vx/shape';
 
-export default function SimpleHorizontalBars({ data, y0, y0Scale, y1Scale, xScale, color, width }) {
 
 
+export default function SimpleHorizontalBars({
+  width, height, margin,
+  data, getX, y0Scale, y1Scale, xScale, colorScale, xMax, axisColor, tickFormat
+}: SimpleDataScaler) {
 
-    return <BarGroupHorizontal<SimpleDataType, SimpleKeyType>
+  return <svg width={width} height={height}>
+    <Group top={margin.top} left={margin.left}>
+      <BarGroupHorizontal<SimpleDataType, SimpleKeyType>
         data={data}
-        y0={y0}
+        y0={getX}
         y0Scale={y0Scale}
         y1Scale={y1Scale}
         xScale={xScale}
-        color={color}
+        color={colorScale}
         keys={SimpleKey}
-        width={width}
-    >
+        width={xMax}
+      >
         {barGroups =>
-            barGroups.map(barGroup => (
-                <Group
-                    key={`bar-group-horizontal-${barGroup.index}-${barGroup.y0}`}
-                    top={barGroup.y0}
-                >
-                    {barGroup.bars.map(bar => (
-                        <Bar
-                            key={`${barGroup.index}-${bar.index}-${bar.key}`}
-                            x={bar.x}
-                            y={bar.y}
-                            width={bar.width}
-                            height={bar.height}
-                            fill={bar.color}
-                            rx={4}
-                        />
-                    ))}
-                </Group>
-            ))
+          barGroups.map(barGroup => (
+            <Group
+              key={`bar-group-horizontal-${barGroup.index}-${barGroup.y0}`}
+              top={barGroup.y0}
+            >
+              {barGroup.bars.map(bar => (
+                <Bar
+                  key={`${barGroup.index}-${bar.index}-${bar.key}`}
+                  x={bar.x}
+                  y={bar.y}
+                  width={bar.width}
+                  height={bar.height}
+                  fill={bar.color}
+                  rx={4}
+                />
+              ))}
+            </Group>
+          ))
         }
-    </BarGroupHorizontal>
+      </BarGroupHorizontal>
+      <AxisLeft
+        scale={y0Scale}
+        stroke={axisColor}
+        tickStroke={axisColor}
+        tickFormat={tickFormat}
+        hideAxisLine
+        tickLabelProps={() => ({
+          fill: axisColor,
+          fontSize: 11,
+          textAnchor: 'end',
+          dy: '0.33em',
+        })}
+      />
+    </Group>
+  </svg>
 }
