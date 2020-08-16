0820d50e824b5cbb31a86c2a36218f742b851955
kj002 <kojih9@gmail.com>
2020-08-16 09:33:52 +0900

    update avro data wrangling

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,27 +1,23 @@
 
-import createAvroReader from './src/utils/readAvroFile';
-const getStream = require('get-stream');
+import readAvroFile from './src/utils/readAvroFile';
+
+function convertPlainObject(objectArray: object[]): object[] {
+    return objectArray.map(element => Object.assign({}, element))
+}
 
 export const onCreateNode = async ({ node, actions }) => {
     const { createNodeField } = actions;
 
-    class Test {
-        id: string;
-        constructor(id) {
-            this.id = id;
-        }
-    }
-
     if (node.internal.type === "File" && node.ext === ".avro") {
-        let data = await getStream.array(createAvroReader(node.absolutePath));
+        let data = await readAvroFile(node.absolutePath);
+        
+        // need to convert to plain object... see:https://github.com/sanity-io/gatsby-source-sanity/issues/25    
+        let out = convertPlainObject(data.filter(element => element.lastUpdate === "2020-08-08"));
 
-        // need to convert to plain object... see:https://github.com/sanity-io/gatsby-source-sanity/issues/25
-        let out = data.map(element => Object.assign({}, element));
         createNodeField({
             node,
             name: "data",
             value: out
         })
-
     }
 }
--- src/utils/readAvroFile.ts
+++ src/utils/readAvroFile.ts
@@ -1,12 +1,16 @@
 const avsc = require('avsc');
 const snappy = require('snappy');
+const getStream = require('get-stream');
 
-export default function createAvroReader(filePath: string) {
-    return avsc.createFileDecoder(filePath, { codecs: {
-        snappy: function (buf, cb) {
-            // Avro appends checksums to compressed blocks, which we skip here.
-            return snappy.uncompress(buf.slice(0, buf.length - 4), cb);
-        }
-    }, codec: 'snappy'
-    })   
+export default async function readAvroFile(filePath: string) {
+    const readStream = avsc.createFileDecoder(filePath, {
+        codecs: {
+            snappy: function (buf, cb) {
+                // Avro appends checksums to compressed blocks, which we skip here.
+                return snappy.uncompress(buf.slice(0, buf.length - 4), cb);
+            }
+        }, codec: 'snappy'
+    });
+
+    return await getStream.array(readStream);
 }
