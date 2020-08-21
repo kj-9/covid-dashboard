12d5c3063338d85920a124e5214103dbd98a9de3
Koji Higuchi <koji@xps13.localdomain>
2020-08-21 22:12:50 +0900

    add seo component

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,15 @@
 export default {
+    siteMetadata: {
+        title: "covid-19-japan-dashboard",
+        titleTemplate: "%s 日本におけるcovid-19ダッシュボード",
+        description: "日本におけるcovid-19ダッシュボード",
+        url: "https://covid-19-japan.netlify.app",
+        image: "", // Path to your image you placed in the 'static' folder
+        twitterUsername: "",
+      },  
     plugins: [
         `gatsby-plugin-sass`,
+        `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-source-filesystem`,
             options: {
