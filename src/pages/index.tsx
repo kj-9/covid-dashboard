00571f4d2bf330a0aac933f0a977ce4a270fd2cf
Koji Higuchi <koji@xps13.localdomain>
2020-08-22 16:22:21 +0900

    update ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import "../styles/bulma.scss";
 import SimpleHorizontalBars from '../components/simpleHorizontalBars';
 import SimpleDataScaler from '../components/simpleDataScaler'
 import SEO from '../components/seo';
+import Footer from '../components/footer';
+
 import { SimpleDataType } from '../components/simpleData';
 import { ParentSize } from '@vx/responsive';
 import { graphql } from 'gatsby';
@@ -28,11 +30,18 @@ export default function Home({ data }) {
   return (
     <div>
       <SEO />
-
-      <div className="columns">
-        <div className="column">
-          <h1 className="title is-1">コロナ重症者数</h1>
-          <h2 className="subtitle is-4">{"更新日: " + formatDate(latestDate)}</h2>
+      <section className="hero is-primary is-bold">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">日本国内 コロナ感染状況ダッシュボード</h1>
+            <h2 className="subtitle">Japan Covid-19 dashboard</h2>
+          </div>
+        </div>
+      </section>
+      <div className="columns is-centered has-background-light">
+        <div className="column is-four-fifths">
+          <h1 className="title is-3">重症者数</h1>
+          <h2 className="subtitle is-5">{"更新日: " + formatDate(latestDate)}</h2>
           <ParentSize>
             {parent => (
               <SimpleHorizontalBars {...new SimpleDataScaler(parent.width, 700, simpleData, 'green', 'black', tickFormat)} />
@@ -40,16 +49,7 @@ export default function Home({ data }) {
           </ParentSize>
         </div>
       </div>
-      <footer className="footer">
-        <div class="content">
-          <h2 className="subtitle is-4">データソース</h2>
-          <ul type="1">
-            <li><a href="https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000164708_00001.html" className="href">厚生労働省報道資料</a></li>
-            <li><a href="https://www.stopcovid19.jp/" className="href">stopcovid19.jp</a>提供の<a href="https://www.stopcovid19.jp/data/covid19japan-all.json" className="href">API</a>を利用させていただいております</li>
-            
-            </ul>
-        </div>
-      </footer>
+      <Footer />
     </div>
 
   )
