4225560d2f44573f2be1ed03013ca126489c9685
kj002 <kojih9@gmail.com>
2020-09-18 21:51:19 +0900

    delete vx dependency

--- src/components/simpleData.tsx
+++
@@ -1,9 +0,0 @@
-export type SimpleDataType = {
-    x: string;
-    y: number;
-}
-export type SimpleKeyType = 'y';
-
-export const SimpleKey = ['y'] as SimpleKeyType[];
-
-export const getX = (d: SimpleDataType) => d.x;
