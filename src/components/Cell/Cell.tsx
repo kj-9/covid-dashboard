6ad071e1e4190615c119cbc85b29b1a0b62017e6
kj002 <kojih9@gmail.com>
2020-10-15 21:46:18 +0900

    update-Cell: refactor code

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -22,25 +22,24 @@ export const Cell: React.FC<CellProps> = ({
   children,
 }) => {
   let arrClassName: Array<string | undefined> = [className]
-  if (textAlign) {
-    switch (textAlign) {
-      case "center":
-        arrClassName.push("has-text-centered")
-        break
-      case "justified":
-        arrClassName.push("has-text-justified")
-        break
-      case "left":
-        arrClassName.push("has-text-left")
-        break
-      case "right":
-        arrClassName.push("has-text-right")
-        break
-      case undefined:
-        break
-      default:
-        throw Error("unkown textAlign: " + textAlign)
-    }
+
+  switch (textAlign) {
+    case "center":
+      arrClassName.push("has-text-centered")
+      break
+    case "justified":
+      arrClassName.push("has-text-justified")
+      break
+    case "left":
+      arrClassName.push("has-text-left")
+      break
+    case "right":
+      arrClassName.push("has-text-right")
+      break
+    case undefined:
+      break
+    default:
+      throw Error("unkown textAlign: " + textAlign)
   }
 
   const joinedClassName: string | undefined =
@@ -48,31 +47,20 @@ export const Cell: React.FC<CellProps> = ({
       ? undefined
       : arrClassName.filter(Boolean).join(" ")
 
-  let outProps: object
+  let outProps: object = { ...props, children: children }
 
   if (joinedClassName && cssObject) {
-    outProps = {
+    outProps = Object.assign(outProps, {
       className: cx(css(cssObject), joinedClassName),
-      ...props,
-      children: children,
-    }
+    })
   } else if (joinedClassName && !cssObject) {
-    outProps = {
+    outProps = Object.assign(outProps, {
       className: joinedClassName,
-      ...props,
-      children: children,
-    }
+    })
   } else if (!joinedClassName && cssObject) {
-    outProps = {
+    outProps = Object.assign(outProps, {
       className: cx(css(cssObject)),
-      ...props,
-      children: children,
-    }
-  } else {
-    outProps = {
-      ...props,
-      children: children,
-    }
+    })
   }
 
   switch (tagType) {
