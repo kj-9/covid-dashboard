5803bf2d3acecb7fccdffd52fac7e1ad1955e0a0
kj002 <kojih9@gmail.com>
2020-11-21 19:58:08 +0900

    update-layout

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,32 +2,13 @@ import React from "react"
 
 export const Footer: React.FC<{}> = () => {
   return (
-    <footer className="footer">
+    <footer className="footer mt-2">
       <div className="content">
-        <h2 className="subtitle is-4">備考</h2>
-        <ul>
-          <li>
-            患者数は
-            <a
-              href="https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000164708_00001.html"
-              className="href"
-            >
-              厚生労働省報道資料
-            </a>
-            より。データ取得に
-            <a href="https://github.com/kaz-ogiwara/covid19" className="href">
-              こちら
-            </a>
-            のGithubレポジトリを利用させて頂いています。
-          </li>
-          <li>
-            本Webサイトはオープンソースです。ソースコードを
-            <a href="https://github.com/kj002/covid-dashboard" className="href">
-              Github
-            </a>
-            で公開しています。
-          </li>
-        </ul>
+        本Webサイトはオープンソースです。ソースコードを
+        <a href="https://github.com/kj002/covid-dashboard" className="href">
+          Github
+        </a>
+        で公開しています。
       </div>
     </footer>
   )
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,12 +1,10 @@
 import React from "react"
-import logo from "../../../static/logo_transparent.png"
 
 export type HeaderProps = {
   title: string
-  subtitle: string
 }
 
-export const Header: React.FC<HeaderProps> = ({ title, subtitle }) => {
+export const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
     <nav
       className="navbar has-background-grey-dark"
@@ -14,14 +12,11 @@ export const Header: React.FC<HeaderProps> = ({ title, subtitle }) => {
       aria-label="main navigation"
     >
       <div className="navbar-brand">
-        <p className="mt-2">
+        <div className="mt-2">
           <span className="title has-text-white-ter has-text-weight-normal is-3 ml-2">
             {title}
           </span>
-          <span className="subtitle has-text-white-ter is-5 ml-2">
-            {subtitle}
-          </span>
-        </p>
+        </div>
       </div>
     </nav>
   )
