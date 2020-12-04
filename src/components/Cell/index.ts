bba171874e9a48ba3d1861a5672a2eb0e845d5eb
kj002 <kojih9@gmail.com>
2020-12-04 22:28:35 +0900

    update-components: delete unused function

--- src/components/Cell/index.ts
+++ src/components/Cell/index.ts
@@ -13,15 +13,3 @@ export const DefaultTooltipProps = {
   flyoutStyle: { fill: "black" },
   flyoutPadding: { left: 15, right: 15 },
 }
-
-import * as scale from "d3-scale"
-
-export const CellDefaultColorScale = (
-  domain: Iterable<scale.NumberValue> = [0, 1]
-) => {
-  const colorRange = ["#e5e7d4", "#ffe570", "#ffa94c", "#ff845f", "#ff0000"]
-
-  return (value: number) => {
-    return scale.scaleQuantize<string>().domain(domain).range(colorRange)(value)
-  }
-}
