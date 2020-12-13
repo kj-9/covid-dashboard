5d41086d410b5fb08954d9ff8bf06a439eaa64fe
kj002 <kojih9@gmail.com>
2020-12-13 23:38:52 +0900

    refactor-Dashboard, Table: merge Table logic to Dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo } from "react"
+import { useTable, useSortBy } from "react-table"
 import { CellSparkline } from "../components/Cell/CellSparkline"
-import { Table } from "./Table"
 import { CellProgressBar } from "./Cell/CellProgressBar"
 import style from "./Dashboard.module.scss"
 
@@ -56,100 +56,130 @@ export const Dashboard = ({
   schema,
   data,
 }: DashboardProps): React.ReactElement => {
-  const [isRendering, setIsRendering] = useState(true)
-  const [prevData, setPrevData] = useState(data)
+  const [isRendering, setIsRendering] = useState<boolean | null>(null)
+  const [prevData, setPrevData] = useState<DashboardData[] | null>(null)
 
+  // Set state if data is changed
   if (data !== prevData) {
     setIsRendering(true)
     setPrevData(data)
   }
 
+  // set isRendering after rendering
   useEffect(() => {
     const id = setTimeout(() => setIsRendering(false), 1)
 
     return () => clearTimeout(id)
   })
 
-  const columns = [
-    {
-      Header: "都道府県",
-      accessor: "entity",
+  // Can not use useMemo inside useMemo. so declare here.
+  const sortType = useMemo(
+    () => (rowA, rowB) => {
+      const valueA = rowA.values.phaseStatusBar
+      const ratioA = valueA.current / valueA.max
+
+      const valueB = rowB.values.phaseStatusBar
+      const ratioB = valueB.current / valueB.max
+
+      if (ratioA === ratioB) return 0
+
+      return ratioA > ratioB ? 1 : -1
     },
+    []
+  )
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "都道府県",
+        accessor: "entity",
+      },
+      {
+        Header: "警戒レベル",
+        accessor: "phase",
+        id: "phaseStatusBar",
+        Cell: ({ value }) => (
+          <CellProgressBar
+            value={value.current}
+            range={value.max}
+            label={`現在のレベル:${value.current}\n最大レベル:${value.max}`}
+            modifiers={[getPhaseStatusModifier(value.current, value.max)]}
+          />
+        ),
+        sortType,
+      },
+      ...schema.indicators.map((indicator, index) => ({
+        Header: indicator.headerLabel,
+        columns: [
+          {
+            id: `indicator_bar_${index}`,
+            accessor: `indicators[${index}].indicator`,
+            Header: "現在",
+            Cell: ({ value }) => (
+              <CellProgressBar
+                value={value}
+                range={indicator.range}
+                modifiers={indicator.barStyle}
+              />
+            ),
+            sortType: "basic",
+          },
+          {
+            id: `indicator_label_${index}`,
+            accessor: `indicators[${index}].indicator`,
+            Cell: ({ value }) => indicator.formatter(value),
+          },
+          {
+            id: `trend_sparkline_${index}`,
+            Header: schema.trendLabel,
+            accessor: `indicators[${index}].trend`,
+            Cell: ({ value }) => (
+              <CellSparkline
+                data={value.map(element => ({
+                  x: element.date,
+                  y: element.value,
+                  label: `${formatMD(
+                    new Date(element.date)
+                  )}時点\n${indicator.formatter(element.value)}`,
+                }))}
+                scale={{ x: "time" }}
+              />
+            ),
+          },
+        ],
+      })),
+    ],
+    [schema]
+  )
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable<DashboardData>(
     {
-      Header: "警戒レベル",
-      accessor: "phase",
-      id: "phaseStatusBar",
-      Cell: ({ value }) => (
-        <CellProgressBar
-          value={value.current}
-          range={value.max}
-          label={`現在のレベル:${value.current}\n最大レベル:${value.max}`}
-          modifiers={[getPhaseStatusModifier(value.current, value.max)]}
-        />
-      ),
-      sortType: useMemo(
-        () => (rowA, rowB) => {
-          const valueA = rowA.values.phaseStatusBar
-          const ratioA = valueA.current / valueA.max
-
-          const valueB = rowB.values.phaseStatusBar
-          const ratioB = valueB.current / valueB.max
-
-          if (ratioA === ratioB) return 0
-
-          return ratioA > ratioB ? 1 : -1
-        },
+      columns,
+      data: useMemo(() => data, [data]),
+      initialState: useMemo(
+        () => ({
+          sortBy: [
+            {
+              id: "indicator_bar_0",
+              desc: true,
+            },
+            {
+              id: "phaseStatusBar",
+              desc: true,
+            },
+          ],
+        }),
         []
       ),
     },
-    ...schema.indicators.map((indicator, index) => ({
-      Header: indicator.headerLabel,
-      columns: [
-        {
-          id: `indicator_bar_${index}`,
-          accessor: `indicators[${index}].indicator`,
-          Header: "現在",
-          Cell: ({ value }) => (
-            <CellProgressBar
-              value={value}
-              range={indicator.range}
-              modifiers={indicator.barStyle}
-            />
-          ),
-          sortType: "basic",
-        },
-        {
-          id: `indicator_label_${index}`,
-          accessor: `indicators[${index}].indicator`,
-          Cell: ({ value }) => indicator.formatter(value),
-        },
-        {
-          id: `trend_sparkline_${index}`,
-          Header: schema.trendLabel,
-          accessor: `indicators[${index}].trend`,
-          Cell: ({ value }) => (
-            <CellSparkline
-              data={value.map(element => ({
-                x: element.date,
-                y: element.value,
-                label: `${formatMD(
-                  new Date(element.date)
-                )}時点\n${indicator.formatter(element.value)}`,
-              }))}
-              scale={{ x: "time" }}
-            />
-          ),
-        },
-      ],
-    })),
-  ]
-
-  const memoColumns = React.useMemo(() => columns, [
-    columns,
-    isRendering,
-    prevData,
-  ])
-  const memoData = React.useMemo(() => data, [data, isRendering, prevData])
+    useSortBy
+  )
 
   if (isRendering) {
     return (
@@ -161,7 +191,29 @@ export const Dashboard = ({
 
   return (
     <div className={style.Dashboard}>
-      <Table<DashboardData> columns={memoColumns} data={memoData} />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => (
+                  <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                ))}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
